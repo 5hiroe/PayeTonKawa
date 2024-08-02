f452b2bcea526acdf1f30e843b12dd9ce726d4ba
シロエ <n.caruelle@icloud.com>
2024-08-02 22:56:47 +0200

    Add 3 db connections

--- orders-service/src/configurations/configuration.js
+++ orders-service/src/configurations/configuration.js
@@ -1,10 +1,9 @@
-/**
- * TODO : Ajouter mongo
- */
+import * as mongo from './mongo.js'
 import * as express from './express.js'
 
 export async function configure (app) {
     await express.configure(app)
+    await mongo.configure(app)
 }
 
 export default configure
